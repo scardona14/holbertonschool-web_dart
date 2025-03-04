186beea70fa6b5c9d9e5e0343a88f82a2973b589
scardona14 <cardanasean123@gmail.com>
2025-03-04 13:36:03 -0400

    Factorial

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,10 +1,6 @@
 int fact(int f) {
-  if (f <= 0)
-    return 0;
+  if (f == 1) return 1;
+  if (f <= 0) return 0;
 
-  if (f == 1)
-    return 1;
-
-  else
-    return f * fact(f - 1);
+  return (f * fact(f - 1));
 }
