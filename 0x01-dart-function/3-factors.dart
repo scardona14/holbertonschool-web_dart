f5c655c0faa9d049c1d9597fa7437cfe01a73921
scardona14 <cardanasean123@gmail.com>
2025-03-04 11:57:43 -0400

    Fixed extra spaces

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,6 +1,5 @@
 int fact(int f) {
   if (f == 1) return 1;
-  if (f <= 0) return 0; // This is wrong but the checkers requires it
-  // By definition, 0! == 1
+  if (f <= 0) return 0; 
   return (f * fact(f - 1));
 }
