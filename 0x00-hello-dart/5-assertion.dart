b03f0bb63c402ae30982a8b4a9c01039715b7dc2
scardona14 <cardanasean123@gmail.com>
2025-03-04 10:12:32 -0400

    Modify the print

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
   int num = int.parse(args[0]);
   assert(num >= 80, 'The score must be bigger or eaqual to 80');
-  print('You passed the exam!');
+  print('You passed');
 }
