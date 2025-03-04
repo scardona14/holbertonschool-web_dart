756bd2e4cf40ec6bb18e7955ba80c46ef35b1f54
scardona14 <cardanasean123@gmail.com>
2025-03-04 13:25:30 -0400

    Changed from double quotes to single

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,6 @@
 void outer(String name, String id) {
   String inner() {
-    int firstSpacePosition = name.indexOf(" ");
+    int firstSpacePosition = name.indexOf('');
     String firstInitial = name[firstSpacePosition + 1];
     String lastName = name.substring(0, firstSpacePosition );
 
