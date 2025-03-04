a7f80cc6853125f72119b4b27c02120dda036135
scardona14 <cardanasean123@gmail.com>
2025-03-04 13:54:51 -0400

    Fixed typo on temperature

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,3 +1,3 @@
 List<double> convertToF(List<double> temperaturesInc) {
-  return temperturesInc.map((temp * 9 / 5 + 32) * 100).round() / 100).toList();
+  return temperaturesInc.map((temp) => ((temp * 9 / 5 + 32) * 100).round() / 100).toList();
 }
