6df1aa25b7d8bc0d5f469a931da5d590fe59a134
scardona14 <cardanasean123@gmail.com>
2025-03-04 13:27:00 -0400

    Deleted commas and extra white spaces

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,10 +1,10 @@
 void outer(String name, String id) {
   String inner() {
-    int firstSpacePosition = name.indexOf('');
+    int firstSpacePosition = name.indexOf(' ');
     String firstInitial = name[firstSpacePosition + 1];
-    String lastName = name.substring(0, firstSpacePosition );
+    String lastName = name.substring(0, firstSpacePosition);
 
-    return('Hello Agent $firstInitial. $lastName, your id is: $id');
+    return('Hello Agent $firstInitial.$lastName your id is: $id');
   }
 
   print(inner());
