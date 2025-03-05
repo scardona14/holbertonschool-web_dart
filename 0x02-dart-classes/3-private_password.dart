19d928ce81c2e39ddfdebd7ff2e40d9169c830f4
scardona14 <cardanasean123@gmail.com>
2025-03-05 15:03:33 -0400

    Private property

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,18 @@
+class Password {
+  String password = '';
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    if (password.length < 8 || _password.length > 16) return false;
+    if (!RegExp(r'[A-Z]').hasMatch(_password)) return false;
+    if (!RegExp(r'[a-z]').hasMatch(_password)) return false;
+    if (!RegExp(r'[0-9]').hasMatch(_password)) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
