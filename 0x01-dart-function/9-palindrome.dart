272ed514d318488fd02c672cd022235c45f4e854
scardona14 <cardanasean123@gmail.com>
2025-03-05 10:31:59 -0400

    Changed the entire code since my first attempt didtn ran

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,16 +1,6 @@
-bool is Palindrome(String s) {
-
-  if (s.length < 3)
+bool isPalindrome(String s) {
+  if (s.length < 3) {
     return false;
-
-    String inReverse = "";
-
-    for (var idx = s.length - 1; idx >= 0; idx--) { 
-      inReverse += s[idx];
-    }
-    if (inReverse == s)
-      return true;
-
-      else
-        return false;
-    }
+  }
+  return s == s.split('').reversed.join('');
+}
