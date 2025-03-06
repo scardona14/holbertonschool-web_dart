daa65e5322396b2ee0b3a7350cb70e16be05584e
scardona14 <cardanasean123@gmail.com>
2025-03-06 15:41:45 -0400

    Deleted data from fetch user

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -2,7 +2,7 @@ import '2-util.dart';
 
 Future<void> getUser() async {
   try {
-    print(await fetchUserData());
+    print(await fetchUser());
   } catch (error) {
     print('error caught: $error');
   }
