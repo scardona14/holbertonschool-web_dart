7166fe76f34bc8f0697d78269874e66753c6f7fc
scardona14 <cardanasean123@gmail.com>
2025-03-06 17:05:00 -0400

    Fixed errors

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,20 +3,20 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    final responce = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
 
-    if (responce.statusCode == 200) {
-      final data = jsonDecode(responce.body);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
 
       if (data['results'] is List) {
-        for (var character in date['results']) {
-          print('$character['name']}');
+        for (var character in data['results']) {
+          print('${character['name']}');
         }
       } else {
         print('Invalid API response format');
       }
     } else {
-      print('Failed to fetch data. Status code: ${responce.statusCode}');
+      print('Failed to fetch data. Status code: ${response.statusCode}');
     }
   } catch (error) {
     print('Error caught: $error');
